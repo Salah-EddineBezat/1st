8c4b4cd7b943161ec5c51ca25badec41c8bbc9c0
Salah-EddineBezat <9025558@student.zadkine.nl>
2025-01-09 12:04:58 +0100

    aanpassing

--- landenOpdracht/app.js
+++ landenOpdracht/app.js
@@ -6,6 +6,7 @@ function submitQuiz() {
         q2: form.querySelector('input[name="q2"]:checked')?.value,
         q3: form.querySelector('input[name="q3"]:checked')?.value,
         q4: form.querySelector('input[name="q4"]:checked')?.value,
+        q5: form.querySelector('input[name="q5"]:checked')?.value,
         q6: form.querySelector('input[name="q6"]:checked')?.value,
         q7: form.querySelector('input[name="q7"]:checked')?.value,
         q8: form.querySelector('input[name="q8"]:checked')?.value,
